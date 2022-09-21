f6871ba1e5ea2fab01bc237792e0250622c8d8eb
ccantill <christoph.cantillon@unifly.aero>
2022-09-21 13:14:16 +0200

    login

--- boot.js
+++ boot.js
@@ -1,11 +1,52 @@
-export class Project {
+function p(params) {
+  return Object.entries(params).map(([k,v]) => `${k}=${encodeURIComponent(v)}`).join("&");
+}
+
+class GitHub {
+  clientId = "c5f22ce7ba042048bdf8";
+  clientSecret = "926eff1f8619d41a462e79ed74acc515488b0505";
+
+  async login() {
+    if (document.location.search.startsWith("?code=")) {
+      await this.exchangeToken(document.location.search.substring(6))
+    } else {
+      document.location = `https://github.com/login/oauth/authorize?scope=user:email&client_id=${this.clientId}`
+    }
+  }
+
+  async exchangeToken(code) {
+    {
+      const params = {
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        code: code,
+        redirectUri: 'https://ccantill.github.io/playground/' //document.location.origin + document.location.pathname
+      }
+      const tokenResponse = await fetch(
+          "https://github.com/login/oauth/access_token?" + p(params), {
+            method: "POST",
+            headers: {
+              'Accept': 'application/json'
+            },
+            mode: 'no-cors'
+          });
+
+      const tokenObj = tokenResponse.json();
+
+      console.log(tokenObj);
+    }
+  }
+}
+
+class Project {
   owner = "unifly-aero"
   project = "unifly-portal"
 
   async listBranches() {
-    const result = await fetch(`https://api.github.com/repos/${this.owner}/${this.project}/branches`)
+    const result = await fetch(
+        `https://api.github.com/repos/${this.owner}/${this.project}/branches`)
     return result
   }
 }
 
-new Project().listBranches().then(r => console.log(r));
+window.github = new GitHub();
